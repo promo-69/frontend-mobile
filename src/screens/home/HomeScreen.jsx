7bf2de3971cd58d1af97677cefa7082dfd6827a4
Daniel M <168379955+mosreztech@users.noreply.github.com>
2026-05-22 08:54:35 -0400

    fix(home): import obsoleto de safeareaview

--- src/screens/home/HomeScreen.jsx
+++ src/screens/home/HomeScreen.jsx
@@ -6,7 +6,6 @@ import {
 } from 'lucide-react-native';
 import {
   Image,
-  SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
@@ -15,6 +14,7 @@ import {
   View,
   ActivityIndicator
 } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { useAuth } from '../../context/AuthContext';
 import { checkHealth } from '../../services/api';
 
